f8ef0b08451dca726ab0589f5bcc538c6d4acc80
codyNog <yoneyoneyone99@gmail.com>
2021-04-03 06:30:19 +0900

    sentry道入

---
+++ src/constants/env/index.ts
@@ -0,0 +1,5 @@
+const SENTRY_DSN = process.env.SENTRY_DSN || "";
+
+export const env = {
+  SENTRY_DSN,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import server from "~/app";
+import * as Sentry from "@sentry/node";
+import { env } from "~/constants/env";
+
+if (env.SENTRY_DSN)
+  Sentry.init({
+    dsn: env.SENTRY_DSN,
+  });
 
 const start = async () => {
   await server.listen(8080, "0.0.0.0").catch((err) => {
